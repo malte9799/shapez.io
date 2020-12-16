263f7dbd533b24567387f5075161fc5d663b5be5
malte9799 <maltem.hilpert@gmx.de>
2020-12-16 18:25:38 +0100

    save item to produce

--- src/js/game/components/item_producer.js
+++ src/js/game/components/item_producer.js
@@ -1,7 +1,27 @@
+import { gItemRegistry } from "../../core/global_registries";
+import { types } from "../../savegame/serialization";
 import { Component } from "../component";
+import { BaseItem } from "../base_item";
+import { typeItemSingleton } from "../item_resolver";
 
 export class ItemProducerComponent extends Component {
     static getId() {
         return "ItemProducer";
+    }
+
+    static getSchema() {
+        return {
+            item: types.nullable(typeItemSingleton),
+        };
+    }
+
+    /**
+     *
+     * @param {object} param0
+     * @param {BaseItem=} param0.item The signal to store
+     */
+    constructor({ item = null }) {
+        super();
+        this.item = item;
     }
 }
