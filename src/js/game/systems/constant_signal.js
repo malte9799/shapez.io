0954a10f08172c5c6b5c2c2eca78c6b8359096e8
malte9799 <maltem.hilpert@gmx.de>
2020-12-16 18:26:15 +0100

    removed log

--- src/js/game/systems/constant_signal.js
+++ src/js/game/systems/constant_signal.js
@@ -103,7 +103,6 @@ export class ConstantSignalSystem extends GameSystemWithFilter {
             }
 
             if (itemInput.chosenItem) {
-                console.log(itemInput.chosenItem);
                 constantComp.signal = itemInput.chosenItem;
             } else {
                 constantComp.signal = this.parseSignalCode(signalValueInput.getValue());
