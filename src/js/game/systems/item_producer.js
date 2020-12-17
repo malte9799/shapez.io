55cc75d0e613f9f33d96327affaa639cce11e079
malte9799 <maltem.hilpert@gmx.de>
2020-12-17 18:14:51 +0100

    preview of the Item to produce

--- src/js/game/systems/item_producer.js
+++ src/js/game/systems/item_producer.js
@@ -1,6 +1,12 @@
 import { ItemProducerComponent } from "../components/item_producer";
 import { GameSystemWithFilter } from "../game_system_with_filter";
 
+import { globalConfig } from "../../core/config";
+import { enumColors } from "../colors";
+import { BaseItem } from "../base_item";
+import { ColorItem, COLOR_ITEM_SINGLETONS } from "../items/color_item";
+import { MapChunkView } from "../map_chunk_view";
+
 export class ItemProducerSystem extends GameSystemWithFilter {
     constructor(root) {
         super(root, [ItemProducerComponent]);
@@ -21,6 +27,34 @@ export class ItemProducerSystem extends GameSystemWithFilter {
             const ejectorComp = entity.components.ItemEjector;
             const ejectItem = (producerComp.item) ? producerComp.item : network.currentValue;
             ejectorComp.tryEject(0, ejectItem);
+        }
+    }
+
+    /**
+     * Draws a given chunk
+     * @param {import("../../core/draw_utils").DrawParameters} parameters
+     * @param {MapChunkView} chunk
+     */
+    drawChunk(parameters, chunk) {
+        const contents = chunk.containedEntitiesByLayer.regular;
+        for (let i = 0; i < contents.length; ++i) {
+            const entity = contents[i];
+            if (entity && entity.components.ItemProducer) {
+                const producerComp = entity.components.ItemProducer;
+                const network = entity.components.WiredPins.slots[0].linkedNetwork;
+
+                if ((!network || !network.hasValue()) && !producerComp.item) continue;
+
+                const value = (producerComp.item) ? producerComp.item : network.currentValue;
+
+                const origin = entity.components.StaticMapEntity.origin;
+                value.drawItemCenteredClipped(
+                    (origin.x + 0.5) * globalConfig.tileSize,
+                    (origin.y + 0.5) * globalConfig.tileSize,
+                    parameters,
+                    25
+                );
+            }
         }
     }
 }
