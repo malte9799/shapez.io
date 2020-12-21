d126bbf6f892cc2cee7740f58abbc5ce5062e95a
malte9799 <maltem.hilpert@gmx.de>
2020-12-21 12:01:11 +0100

    edit Level

--- src/js/game/levels/level_4.js
+++ src/js/game/levels/level_4.js
@@ -9,13 +9,13 @@ export class Level_4 extends Level {
     }
 
     getDimensions() {
-        return new Vector(7, 6);
+        return new Vector(6, 6);
     }
 
     // { root, origin, rotation, originalRotation, rotationVariant, variant }
     setupLevel(root) {
         return [
-            ["hub", new Vector(6, 4), "left", 0, 0, "default"],
+            ["hub", new Vector(5, 3), "left", 0, 0, "default"],
             ["item_producer", new Vector(0, 1), "right", 0, 0, "default", "RuRuRuRu"],
             ["item_producer", new Vector(2, 5), "top", 0, 0, "default", "blue"],
             ["item_producer", new Vector(3, 5), "top", 0, 0, "default", "green"],
