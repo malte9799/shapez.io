74f3cdd1e020359951cd59949e5834c4668a5ffc
Malte9799 <maltem.hilpert@gmx.de>
2021-02-13 16:01:02 +0100

    Added a lot of flexibility for levels

--- src/js/game/level.js
+++ src/js/game/level.js
@@ -1,12 +1,6 @@
 import { globalConfig } from "../core/config";
-import { Loader } from "../core/loader";
-import { AtlasSprite } from "../core/sprites";
 import { Vector } from "../core/vector";
-import { SOUNDS } from "../platform/sound";
-import { StaticMapEntityComponent } from "./components/static_map_entity";
-import { Entity } from "./entity";
 import { GameRoot } from "./root";
-import { getCodeFromBuildingData } from "./building_codes";
 import { gMetaBuildingRegistry } from "../core/global_registries";
 
 import trim from "trim";
@@ -14,24 +8,25 @@ import { enumColors } from "./colors";
 import { BOOL_FALSE_SINGLETON, BOOL_TRUE_SINGLETON } from "./items/boolean_item";
 import { COLOR_ITEM_SINGLETONS } from "./items/color_item";
 import { ShapeDefinition } from "./shape_definition";
-
+import { BaseItem } from "../game/base_item";
 
 const rotation = {
-    "top": 0,
-    "right": 90,
-    "bottom": 180,
-    "left": 270
-}
+    top: 0,
+    right: 90,
+    bottom: 180,
+    left: 270,
+};
 
 export class Level {
     /**
      *
      * @param {string} id Level id
-     * @param {num} lvlNum Level number
+     * @param {number} lvlNum Level number
      */
     constructor(id, lvlNum) {
         this.id = id;
         this.lvlNum = lvlNum;
+        this.root = null;
     }
 
     /**
@@ -41,26 +36,75 @@ export class Level {
         return this.id;
     }
 
+    /**
+     * Returns the Origin chunk tile of the level
+     */
+    getOriginChunk() {
+        return new Vector((this.lvlNum % 5) * 2, Math.floor(this.lvlNum / 5) * 2);
+    }
+
+    /**
+     * Returns the Origin tile of the level (upper left corner)
+     */
     getOrigin() {
-        const originChunk = new Vector((this.lvlNum % 5) * 2, Math.floor(this.lvlNum/5) * 2);
+        const center = this.getCenterTile();
         const dimensions = this.getDimensions();
-        const x = originChunk.x * globalConfig.mapChunkSize + globalConfig.mapChunkSize - Math.round(dimensions.x / 2);
-        const y = originChunk.y * globalConfig.mapChunkSize + globalConfig.mapChunkSize - Math.round(dimensions.y / 2);
+        const x = center.x - Math.round(dimensions.x / 2);
+        const y = center.y - Math.round(dimensions.y / 2);
         return new Vector(x, y);
     }
 
-    getCenter() {
-        const originChunk = new Vector((this.lvlNum % 5) * 2, Math.floor(this.lvlNum/5) * 2);
-        const dimensions = this.getDimensions();
-        return new Vector(originChunk.x * globalConfig.mapChunkSize, originChunk.y * globalConfig.mapChunkSize);
+    /**
+     * Returns the Center tile of a level
+     */
+    getCenterTile() {
+        const originChunk = this.getOriginChunk();
+        return new Vector(
+            originChunk.x * globalConfig.mapChunkSize + globalConfig.mapChunkSize,
+            originChunk.y * globalConfig.mapChunkSize + globalConfig.mapChunkSize
+        );
     }
 
+    /**
+     * Returns the Real Center of a level (might be of by half a tile)
+     */
+    getRealCenter() {
+        const origin = this.getOrigin();
+        const dim = this.getDimensions();
+        return new Vector(origin.x + dim.x / 2, origin.y + dim.y / 2);
+    }
+
+    /**
+     * Tests if the tile is inside a level
+     * @param {Vector} tile
+     */
     isLvlTiles(tile) {
-        const lvlOrigin = this.getOrigin()
+        const lvlOrigin = this.getOrigin();
         const dimensions = this.getDimensions();
 
-        if (tile.x < lvlOrigin.x + 1 || tile.y < lvlOrigin.y + 1 || tile.x > lvlOrigin.x + dimensions.x - 2 || tile.y > lvlOrigin.y + dimensions.y - 2) return false
-        return true
+        if (
+            tile.x < lvlOrigin.x + 1 ||
+            tile.y < lvlOrigin.y + 1 ||
+            tile.x > lvlOrigin.x + dimensions.x - 2 ||
+            tile.y > lvlOrigin.y + dimensions.y - 2
+        )
+            return false;
+        return true;
+    }
+
+    isLvlLoaded() {
+        return this.root.map.getTileContent(this.getOrigin(), "regular") != null;
+    }
+
+    /**
+     * Should setup/load the Level and move Camera to it
+     * @param {GameRoot} root
+     */
+    load(root) {
+        this.root = root;
+        if (!this.isLvlLoaded()) this.createLevel();
+        root.camera.setDesiredCenter(this.getRealCenter().toWorldSpace());
+        root.camera.setDesiredZoom(1);
     }
 
     /**
@@ -72,39 +116,37 @@ export class Level {
 
     /**
      * Should setup the Level
-     * @param {Level} level
      * @param {GameRoot} root
+     * @returns {any}
      */
-    setupLevel(level, root) {
+    setupLevel(root) {
         abstract;
     }
 
     /**
      * Creates the Level and placing it
-     * @param {object} param0
-     * @param {GameRoot} param0.root
      */
-    createLevel(root) {
-        const origin = this.getOrigin()
+    createLevel() {
+        const origin = this.getOrigin();
 
         // place special buildings
-        const lvlBuildings = this.setupLevel(root)
+        const lvlBuildings = this.setupLevel(this.root);
         for (let i in lvlBuildings) {
-            const param = lvlBuildings[i]
+            const param = lvlBuildings[i];
             let entity = gMetaBuildingRegistry.findById(param[0]).createEntity({
                 root: this.root,
                 origin: new Vector(origin.x + param[1].x, origin.y + param[1].y),
                 rotation: rotation[param[2]],
                 originalRotation: param[3],
                 rotationVariant: param[4],
-                variant: param[5]
-            })
+                variant: param[5],
+            });
             if (param.length >= 7 && param[0] == "item_producer") {
-                entity.components.ItemProducer.item = this.parseSignalCode(root, param[6]);
+                entity.components.ItemProducer.item = this.parseSignalCode(this.root, param[6]);
             }
-            root.map.placeStaticEntity(entity);
-            root.entityMgr.registerEntity(entity);
-        };
+            this.root.map.placeStaticEntity(entity);
+            this.root.entityMgr.registerEntity(entity);
+        }
 
         // place walls
         const dimensions = this.getDimensions();
@@ -113,18 +155,18 @@ export class Level {
                 if (y != 0 && y != dimensions.y - 1 && x != 0 && x != dimensions.x - 1) {
                     continue;
                 }
-                const tile = new Vector(x + origin.x, y + origin.y)
-                if (!root.map.getTileContent(tile, "regular")) {
+                const tile = new Vector(x + origin.x, y + origin.y);
+                if (!this.root.map.getTileContent(tile, "regular")) {
                     let entity = gMetaBuildingRegistry.findById("wall").createEntity({
                         root: this.root,
                         origin: tile,
                         rotation: 0,
                         originalRotation: 0,
                         rotationVariant: 0,
-                        variant: "default"
+                        variant: "default",
                     });
-                    root.map.placeStaticEntity(entity);
-                    root.entityMgr.registerEntity(entity);
+                    this.root.map.placeStaticEntity(entity);
+                    this.root.entityMgr.registerEntity(entity);
                 }
             }
         }
@@ -160,5 +202,5 @@ export class Level {
         }
 
         return null;
-    };
+    }
 }
